72c44a290d2dddf04cedff1a4df5073a6472f3a7
Thomas Yuan <xty20@cl.cam.ac.uk>
2024-01-12 13:16:59 +0000

    update tto model

--- models/stable_diffusion.py
+++ models/stable_diffusion.py
@@ -1,51 +1,24 @@
 import torch
 import pytorch_lightning as pl
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline
 
 class StableDiffusionModule(pl.LightningModule):
-    def __init__(self, split_gpus = True):
+    def __init__(self, split_gpus = False):
         super().__init__()
         self.split_gpus = split_gpus
-        self.base_model = DiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-xl-base-1.0", 
+        self.model = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", 
             torch_dtype=torch.float16
         )
-        if split_gpus:
-            self.base_model.cuda(0)
-        self.base_model.unet = torch.compile(self.base_model.unet, mode="reduce-overhead", fullgraph=True)
-
-        self.refiner_model = DiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-xl-refiner-1.0",
-            text_encoder_2=self.base_model.text_encoder_2,
-            vae=self.base_model.vae,
-            torch_dtype=torch.float16
-        )
-        if split_gpus:
-            self.base_model.cuda(1)
+        self.model.unet = torch.compile(self.model.unet, mode="reduce-overhead", fullgraph=True)
         self.n_steps = 40
         self.high_noise_frac = 0.8
 
     def forward(self, x):
-        print(self.base_model.device)
-        print(self.refiner_model.device)
-        import pdb
-        pdb.set_trace()
         image = self.base_model(
             prompt=x,
             num_inference_steps=self.n_steps,
-            denoising_end=self.high_noise_frac,
-            output_type="latent",
-        ).images
-
-        if self.split_gpus:
-            image = image.cuda(1)
-
-        image = self.refiner_model(
-            prompt=x,
-            num_inference_steps=self.n_steps,
-            denoising_start=self.high_noise_frac,
-            image=image,
-        ).images[0]
+        )[0]
         
         return image
     
